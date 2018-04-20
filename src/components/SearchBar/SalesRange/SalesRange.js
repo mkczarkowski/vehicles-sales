6b6379317f8c866033a1913b076bd86052ddf2f8
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-04-20 18:30:57 +0200

    Turn class cmp to functional cmp

--- src/components/SearchBar/SalesRange/SalesRange.js
+++ src/components/SearchBar/SalesRange/SalesRange.js
@@ -2,29 +2,19 @@ import React, { Component } from 'react';
 
 import 'rc-slider/assets/index.css';
 import Slider from 'rc-slider';
-import {inputSize} from "../../../shared/constants";
+import { inputSize } from '../../../shared/constants';
 
 const { Range } = Slider;
 
-class SalesRange extends Component {
-  state = {
-    lowerBound: 20,
-    upperBound: 40,
-    value: [20, 40],
-  };
-
-  render() {
-    return (
-      <div>
-        <Range
-          style={{ width: inputSize }}
-          allowCross={false}
-          value={this.props.selectValue}
-          onChange={this.props.handleChange}
-        />
-      </div>
-    );
-  }
-}
+const SalesRange = ({ selectValue, handleChange }) => (
+  <div>
+    <Range
+      style={{ width: inputSize }}
+      allowCross={false}
+      value={selectValue}
+      onChange={handleChange}
+    />
+  </div>
+);
 
 export default SalesRange;
