c2cd24b403bdb8ae8f848491cc37490970b0543d
Marcin Czarkowski <marcinczarkowski@onet.eu>
2018-04-20 18:11:27 +0200

    Store constants in separate module

---
+++ src/shared/constants.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+
+export const INPUT_TYPE = Object.freeze({
+  INPUT_TYPE_COUNTRY: 'SELECT_COUNTRY',
+  INPUT_TYPE_YEAR: 'SELECT_YEAR',
+  INPUT_TYPE_SALES_RANGE: 'SELECT_SALES_RANGE',
+});
